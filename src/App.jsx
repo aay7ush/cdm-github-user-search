febf388c4c4f32fa915beb909b53f7c956d93006
aay7ush <aay7ush@gmail.com>
2024-02-21 13:00:07 +0530

    feat: add starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import SearchBox from './components/SearchBox'
-import UserProfile from './components/UserProfile'
-
 const App = () => {
-  const [user, setUser] = useState('aay7ush')
-
   return (
-    <main className="min-h-screen text-white grid place-content-center dark:bg-gray-900 space-y-5 py-10">
-      <Header />
-      <SearchBox setUser={setUser} />
-      <UserProfile user={user} />
-    </main>
+    <h1 className="text-3xl font-bold underline">
+      Let's build Github User Search App
+    </h1>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,4 @@
-import ThemeSwitcher from './ThemeSwitcher'
-
 const Header = () => {
-  return (
-    <header className="flex justify-between items-center">
-      <h1 className="text-2xl font-bold text-black dark:text-white">
-        devSearch
-      </h1>
-      <ThemeSwitcher />
-    </header>
-  )
+  return <div>Header</div>
 }
 export default Header
